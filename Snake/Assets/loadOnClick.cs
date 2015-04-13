199b79d7c83b80b1ade0f7a23df3d47e4c77c143
William Yang <williamyang93@gmail.com>
2015-04-12 23:40:17 -0700

    Added title
    Added title, trying to add game over

---
+++ Snake/Assets/loadOnClick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadOnClick : MonoBehaviour {
+
+	public void LoadScene(int level)
+	{
+		// Will take in an integer that says what scene it's taking in
+		Application.LoadLevel(level);
+		
+	}
+}

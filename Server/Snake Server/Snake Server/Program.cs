070d28f0e5c0151ec76379c8ea2558d38eeed4d9
Victor Hsiao <vwhsiao@uci.edu>
2015-04-18 14:28:53 -0700

    Added Server Folder

---
+++ Server/Snake Server/Snake Server/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alchemy.Classes;
+
+
+namespace Snake_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- Snake/Assets/Snake.cs
+++ Snake/Assets/Snake.cs
@@ -6,6 +6,7 @@ using System.Timers;
 using UnityEngine.UI;
 
 
+
 public class Snake : MonoBehaviour {
 	//List for tails
 	List<Transform> tail = 	new List<Transform>();
